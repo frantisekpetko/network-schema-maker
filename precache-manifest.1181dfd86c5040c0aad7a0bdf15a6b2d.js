8e236746074e756948228a201f37646d7f2a567d
František Petko <frantisek.petko7@seznam.cz>
2019-04-10 14:10:20 +0200

    Updates

---
+++ precache-manifest.1181dfd86c5040c0aad7a0bdf15a6b2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8adfb927ae75906a41b",
+    "url": "/network-schema-maker/static/js/runtime~main.c8adfb92.js"
+  },
+  {
+    "revision": "f817cd2c2095e96ffa47",
+    "url": "/network-schema-maker/static/js/main.f817cd2c.chunk.js"
+  },
+  {
+    "revision": "317ee9d9bf3e14878d67",
+    "url": "/network-schema-maker/static/js/2.317ee9d9.chunk.js"
+  },
+  {
+    "revision": "f817cd2c2095e96ffa47",
+    "url": "/network-schema-maker/static/css/main.6d85d20f.chunk.css"
+  },
+  {
+    "revision": "1d7266cd9270ce23684f4bbb968d2cc9",
+    "url": "/network-schema-maker/index.html"
+  }
+];
